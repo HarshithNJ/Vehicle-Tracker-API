173936aeaa0f7f54faaae5d726b8c7866bfa818e
Harshith N J <njharshith122@gmail.com>
2025-01-24 12:04:28 +0530

    project Started

---
+++ src/main/java/org/vehicle_tracker/vehicle_tracker/VehicleTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.vehicle_tracker.vehicle_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vehicle_tracker/vehicle_tracker/VehicleTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vehicle_tracker.vehicle_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
