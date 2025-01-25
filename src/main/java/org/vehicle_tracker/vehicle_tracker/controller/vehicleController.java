6eaaafb47fbd7699b4f0d532b8e54c53022f38ab
Harshith N J <njharshith122@gmail.com>
2025-01-25 16:11:52 +0530

    save data

--- src/main/java/org/vehicle_tracker/vehicle_tracker/controller/vehicleController.java
+++ src/main/java/org/vehicle_tracker/vehicle_tracker/controller/vehicleController.java
@@ -1,9 +1,14 @@
 package org.vehicle_tracker.vehicle_tracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vehicle_tracker.vehicle_tracker.dto.vehicle;
 import org.vehicle_tracker.vehicle_tracker.service.vehicleService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -12,5 +17,13 @@ public class vehicleController {
 
     @Autowired
     vehicleService service;
+
+
+    //To Create a new Vehicle Detail
+    @Operation(summary = "Add a new vehicle Record", description = "To Create a new vehicle Record")
+    @GetMapping("/vehicles")
+    public ResponseEntity<Object> addVehicle(@RequestBody vehicle vehicle){
+        return service.addVehicle(vehicle);
+    }
     
 }
--- src/main/java/org/vehicle_tracker/vehicle_tracker/service/vehicleService.java
+++ src/main/java/org/vehicle_tracker/vehicle_tracker/service/vehicleService.java
@@ -1,7 +1,13 @@
 package org.vehicle_tracker.vehicle_tracker.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.vehicle_tracker.vehicle_tracker.dto.vehicle;
 import org.vehicle_tracker.vehicle_tracker.repository.vehicleRepository;
 
 @Service
@@ -9,5 +15,23 @@ public class vehicleService {
 
     @Autowired
     vehicleRepository repository;
+
+    public ResponseEntity<Object> addVehicle(vehicle vehicle) {
+
+        if(repository.existsByVehicleID(vehicle.getVehicleID())){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error","Vehicle Already Exists with the Vehicle ID" + vehicle.getVehicleID());
+
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_ACCEPTABLE);
+        }else{
+            repository.save(vehicle);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("success", "Vehicle Added Successfully");
+            map.put("Data", vehicle);
+
+            return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+        }
+    }
     
 }
